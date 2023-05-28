c2a21881e31b357506f237623883d93f17b5f2a7
Yaroslav Pazynenko <yaroslavpazynenko@gmail.com>
2023-05-28 18:03:03 +0300

    fixed header layout

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,8 @@
-import styles from "./Header.module.css";
 import SearchIco from "../../assets/Search.svg";
+import Notification from "../../assets/Notification.svg";
+import ArrowDown from "../../assets/ArrowDown.svg";
+import Avatar from "../../assets/Avatar.png";
+import styles from "./Header.module.css";
 const Header = () => {
   return (
     <div className={styles.header}>
@@ -11,8 +14,18 @@ const Header = () => {
       </form>
       <div className={styles.wrapper}>
         <div className={styles.wrapperNotify}>
-          <div className={styles.language}>EN</div>
-          <div className={styles.notify}>N</div>
+          <div className={styles.selectContainer}>
+            <select className={styles.selectBox}>
+              <option value="UA">UA</option>
+              <option value="EN">EN</option>
+            </select>
+            <div className={styles.iconContainer}>
+              <img src={ArrowDown} alt="ArrowDown" />
+            </div>
+          </div>
+          <button className={styles.notify}>
+            <img src={Notification} alt="Notification" />
+          </button>
         </div>
 
         <div className={styles.profile}>
@@ -20,7 +33,11 @@ const Header = () => {
             <div className={styles.userName}>Peter</div>
             <div className={styles.userRole}>Admin</div>
           </div>
-          <div className={styles.avatar}></div>
+          <div className={styles.avatar}>
+            <img className={styles.avatar} src={Avatar} alt="Avatar" />
+            <span className={styles.badgeCircle}></span>
+            <span className={styles.badge} data-color-badge="#67d37f"></span>
+          </div>
         </div>
       </div>
     </div>
